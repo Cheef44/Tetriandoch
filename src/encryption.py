6380801f938e095ab589cefd1bbeba254ae3ad39
Cheef44 <bananchik2211@mail.ru>
2024-02-10 00:18:02 +0300

    feature: adding an encryption class for the text entered in the string. In this class, the synchronous key is decrypted and the entered text is encrypted into the directory specified by the user

---
+++ src/encryption.py
@@ -0,0 +1,45 @@
+from Crypto.Cipher import AES, PKCS1_OAEP
+from Crypto.PublicKey import RSA
+import os
+
+class EncrtptionText:
+    global iv
+    if not os.path.exists('data/vectore.iv'):
+        with open('data/vectore.iv', 'wb') as vectore:
+            g = os.urandom(16)
+            vectore.write(g)
+            
+    with open('data/vectore.iv', 'rb') as iv:
+        iv = iv.read()
+    
+    def __init__(self, text:str, enc_path, file, path_async_key='data/asynckey', path_sync_key='data/synckey.pub') -> None:
+        self.text = text
+        self.enc_path = enc_path
+        self.file = file
+        self.path_async_key = path_async_key
+        self.path_sync_key = path_sync_key
+    
+    def decrypt_sync_key(self):
+        with open(self.path_async_key, 'rb') as private_key:
+            private_key = private_key.read()
+            private_key = RSA.import_key(private_key)
+            
+        with open(self.path_sync_key, 'rb') as sync_key:
+            sync_key = sync_key.read()
+            
+        decrypt = PKCS1_OAEP.new(private_key)
+        decrypt = decrypt.decrypt(sync_key)
+        
+        return decrypt
+    
+    def encryption_text(self):
+        encrypt = AES.new(self.decrypt_sync_key(), AES.MODE_CFB, iv)
+        encrypt_text = encrypt.encrypt(bytes(self.text, encoding='utf8'))
+        if not os.path.exists(self.enc_path):
+            os.mkdir(self.enc_path)
+            with open(f'{self.enc_path}/{self.file}.aes', 'wb') as encrypt_file:
+                encrypt_file.write(encrypt_text)
+                
+            return f'Your encrypted file is in {self.enc_path}'
+        else:
+            return f'The entered directory exists'
